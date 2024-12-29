0a4a535a16530b7420e6c61eb845bfecf533c47a
fbicandy <fredybicandy085@gmail.com>
2024-12-29 20:59:58 +0200

    Did styles,routes,components,connection

---
+++ pages/nutriverse/request_form.php
@@ -0,0 +1,93 @@
+<?php include("../../php/components/material_nutriverse.php"); ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>NutriVerse</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+    <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+    <style>
+        .primary {
+            color: #f7c761;
+            font-family: Geomanist Medium, sans-serif;
+        }
+
+        .text {
+            color: #222222;
+            font-family: Geomanist Medium, sans-serif;
+        }
+
+        .accent {
+            color: #F2E8CF;
+        }
+    </style>
+</head>
+
+<body class="bg-gradient-to-r from-[#eaedf3] to-[#fafcff] min-h-screen">
+    <!-- Initialize AOS -->
+    <script>
+        AOS.init();
+    </script>
+
+    <!-- Navbar -->
+    <?= nutrinavbar("bg-gradient-to-r from-[#eaedf3] to-[#fafcff]") ?>
+    <!-- Main Section -->
+    <main class="min-h-screen">
+        <!-- Welcome Screen -->
+        <section
+            class="container mx-auto px-6 md:px-12 py-12 flex flex-col lg:flex-row items-center lg:justify-between">
+            <div class="lg:w-1/2 lg:mb-32 text-center lg:text-left" data-aos="fade-up">
+                <h1 class="text text-4xl md:text-6xl font-semibold leading-tight">
+                    Ready to elevate your <span class="text-white bg-[#f7c761] px-2">well-being?</span>
+                </h1>
+                <p class="mt-6 text-lg mb-12">
+                    Just one step is enough to find out more about <br>
+                    <b> how you can change the yout live </b> with healthy<br>
+                    eating habits.
+                </p>
+            </div>
+            <!-- Welcome Image -->
+            <div data-aos="fade-up" class="lg:w-1/2 lg:mt-32 min-h-fit">
+                <form class="max-w-md mx-auto space-y-4">
+                    <input required type="text" placeholder="Name*"
+                        class="w-full px-5 py-3 border rounded-md shadow-md focus:ring-2 focus:ring-[#f7c761] focus:outline-none bg-white text-[#8290ac]">
+                    <input required type="text" placeholder="Last name*"
+                        class="w-full px-4 py-3 border rounded-md shadow-md focus:ring-2 focus:ring-[#f7c761] focus:outline-none bg-white text-[#8290ac]">
+                    <input required type="number" placeholder="Age*"
+                        class="w-full px-4 py-3 border rounded-md shadow-md focus:ring-2 focus:ring-[#f7c761] focus:outline-none bg-white text-[#8290ac]">
+                    <input required type="email" placeholder="Email*"
+                        class="w-full px-4 py-3 border rounded-md shadow-md focus:ring-2 focus:ring-[#f7c761] focus:outline-none bg-white text-[#8290ac]">
+                    <select
+                        class="w-full px-4 py-3 border rounded-md shadow-md focus:ring-2 focus:ring-[#f7c761] focus:outline-none bg-white text-[#8290ac]">
+                        <option value="" disabled selected>Country</option>
+                        <option value="LB">Lebanon 🇱🇧</option>
+                        <option value="QA">Qatar 🇶🇦</option>
+                        <option value="AE">UAE 🇦🇪</option>
+                    </select>
+                    <textarea placeholder="Objective (Optional)"
+                        class="w-full px-4 py-3 border rounded-md shadow-md focus:ring-2 focus:ring-[#f7c761] focus:outline-none bg-white text-[#8290ac]"></textarea>
+                    <div class="grid grid-cols-2 gap-4">
+                        <input type="number" placeholder="country code*"
+                            class="w-full px-4 py-3 border rounded-md shadow-md focus:ring-2 focus:ring-[#f7c761] focus:outline-none bg-white text-[#8290ac]">
+                        <input type="tel" placeholder="Mobile phone*"
+                            class="w-full px-4 py-3 border rounded-md shadow-md focus:ring-2 focus:ring-[#f7c761] focus:outline-none bg-white text-[#8290ac]">
+                    </div>
+                    <button type="submit"
+                        class="ml-16 bg-[#f7c761] text-white px-32 py-3 rounded-full text-sm font-medium hover:bg-transparent hover:text-[#363c48] hover:border-[#f7c761] border duration-300">
+                        Send Request</button>
+                </form>
+
+            </div>
+        </section>
+
+    </main>
+
+
+    <!-- Footer -->
+    <?= footer() ?>
+</body>
+
+</html>
---
+++ php/components/material_nutriverse.php
@@ -0,0 +1,181 @@
+<?php
+
+function nutrinavbar($style)
+{
+    $nutriverse_home = "home.php";
+    $nutriverse_about = "about.php";
+    $nutriverse_blog = "../blog.php";
+    $nutriverse_request = "request_form.php";
+    print (" 
+    <script>
+        AOS.init();
+    </script>
+    <nav class='{$style} px-6 py-4 md:px-12'>
+        <div class='container mx-auto flex items-center justify-between'>
+            <h1 data-aos='fade-up' class='sm:block hidden ml-32 primary text-5xl font-semibold leading-normal'>
+                <a href='{$nutriverse_home}'><strong>NutriVerse</strong></a>
+            </h1>
+            <div class='hidden md:flex space-x-6 items-center'>
+                <a href='{$nutriverse_about}' class='no-underline text px-2'><b>About</b></a>
+                <a href='{$nutriverse_blog}' class='no-underline text px-2'><b>Blog</b></a>
+                <a href='{$nutriverse_request}'
+                    class='bg-[#f7c761] text-white px-4 py-2 rounded-full text-sm font-medium hover:bg-transparent hover:text-[#363c48] hover:border-[#f7c761] border duration-300'>
+                    <b>Request Consultation</b>
+                </a>
+            </div>
+            <div class='md:hidden'>
+                <button id='menu-toggle' class='focus:outline-none'>
+                    <svg class='w-6 h-6' fill='none' stroke='currentColor' viewBox='0 0 24 24'
+                        xmlns='http://www.w3.org/2000/svg'>
+                        <path stroke-linecap='round' stroke-linejoin='round' stroke-width='2'
+                            d='M4 6h16M4 12h16m-7 6h7'></path>
+                    </svg>
+                </button>
+            </div>
+        </div>
+
+
+        <!-- Mobile Navbar Version -->
+        <div id='mobile-menu' class='hidden md:hidden mt-4'>
+            <a href='{$nutriverse_home}' class='block no-underline text px-2'><b>Home</b></a>
+            <hr>
+            <a href='{$nutriverse_about}' class='mt-5 block no-underline text px-2'><b>About</b></a>
+            <hr>
+            <a href='{$nutriverse_blog}' class='mt-5 block no-underline text px-2'><b>Blog</b></a>
+            <hr>
+            <a href='{$nutriverse_request}' class='mt-5 block bg-[#f7c761] text-white px-4 py-2 rounded-full'><b>Request
+                    Consultation</b></a>
+            <hr>
+        </div>
+    </nav>
+    ");
+}
+
+function features_card($delay, $title, $highlited, $describe)
+{
+    print ("<div data-aos='zoom-in' data-aos-delay='{$delay}'
+                    class='bg-white rounded-xl shadow-lg p-8 max-w-80 py-11 mx-auto inline-block mr-5 mt-7'>
+                    <h2 class='text-2xl text-gray-800 font-bold'>
+                    {$title} <span class='bg-[#F2E8CF] text-gray-800 px-2'>{$highlited}</span>
+                    </h2>
+                    <p class='mt-4 text text-center'>
+                        {$describe}</b>
+                    </p>
+                </div>");
+}
+
+function side_image($image)
+{
+    print ("
+    <div class='lg:w-1/2 mt-8 lg:mt-0'>
+        <img src='{$image}'class='hidden sm:block w-full max-w-lg mx-auto'>
+    </div>");
+}
+
+function feedback_card($delay, $image, $feedback, $name)
+{
+    print (
+        "<div data-aos='zoom-in' data-aos-delay='{$delay}' class='p-4 shadow-lg rounded-xl'>
+        <img src='{$image}'
+        class='mx-auto'>
+        <p class='text-center mt-4 text-sm md:text-base'>
+        <span class='bg-[#f0ffff] px-2'>
+        $feedback<br><strong>$name</strong>
+        </span>
+        </p>
+        </div>"
+    );
+}
+
+function footer()
+{
+    print ('
+    <footer class="bg-gray-800 py-6">
+    <div class="container mx-auto px-6 md:px-12 text-center text-gray-300">
+    <p>&copy; 2024 NutriVerse. All Rights Reserved.</p>
+    </div>
+    </footer>  
+    
+    <script>
+        const menuToggle = document.getElementById("menu-toggle");
+        const mobileMenu = document.getElementById("mobile-menu");
+
+        menuToggle.addEventListener("click", () => {
+            mobileMenu.classList.toggle("hidden");
+        });
+    </script>
+    ');
+}
+
+function FAQ()
+{
+    print ('
+    <!-- FAQ -->
+    <section align="center" class="container mx-auto px-6 md:px-12 py-12 items-center lg:justify-between">
+        <!-- Title -->
+        <div class="text-center">
+            <h2 class="text text-center text-4xl font-semibold leading-normal">
+                Still have questions? We have the answers
+            </h2>
+        </div>
+        <!-- Q1 -->
+        <div data-aos="zoom-in"
+            class="lg:w-1/2 text-center lg:text-left bg-[#eef3fb] rounded-xl shadow-lg p-5 py-11 w-fit mx-auto inline-block mr-5 mt-16">
+            <h2 class="text  font-semibold leading-normal">
+                How can I access the nutritional monitoring?
+            </h2>
+            <p data-aos="zoom-in" class="mt-5 px-2 text-gray-700">
+                You can access Nutrium Care in three ways: if you have a Gympass subscription, if your company
+                enjoys the Nutrium Care
+                benefit,<br>
+                or if you visit a dietitian who uses this platform.<br>
+                <br>
+                <br>
+                To access nutritional monitoring through Gympass, you must have an active subscription at the
+                following levels:<br>
+                <br>
+            <ul align="left" class="ml-12">
+                <li class="list-disc">
+                    Silver subscription or higher - Brazil, Mexico and the United States;
+                </li>
+                <li class="list-disc">
+                    Gold subscription or higher - Argentina, Chile, Spain, Italy, Ireland and the United Kingdom;
+                </li>
+                <li class="list-disc">
+                    Premium subscription or higher - Germany.
+                </li>
+            </ul>
+
+            </p>
+        </div>
+        <!-- Q2 -->
+
+        <div data-aos="zoom-in"
+            class="lg:w-1/2 text-center lg:text-left bg-[#eef3fb] rounded-xl shadow-lg p-5 py-11 w-fit mx-auto inline-block mr-5 mt-8">
+            <h2 class="text  font-semibold leading-normal">
+                How does NutriVerse analyze my food?
+            </h2>
+            <p data-aos="zoom-in" class="mt-5 px-2 text-gray-700">
+                NutriVerse uses advanced machine learning technology to analyze your food plate images. Once you
+                upload a photo,our
+                system
+                identifies the ingredients, calculates approximate calorie content, and provides detailed
+                nutritional information.
+            </p>
+        </div>
+        <!-- Q3 -->
+        <div data-aos="zoom-in"
+            class="lg:w-1/2 text-center lg:text-left bg-[#eef3fb] rounded-xl shadow-lg p-5 py-11 w-fit mx-auto inline-block mr-5 mt-8">
+            <h2 class="text  font-semibold leading-normal">
+                Can I use NutriVerse without a dietician?
+            </h2>
+            <p data-aos="zoom-in" class="mt-5 px-2 text-gray-700">
+                Yes! Individuals can use NutriVerse independently to log meals, receive nutritional insights,
+                and set personal goals.
+                However, for
+                professional guidance, connecting with a dietician through our platform is highly recommended.
+            </p>
+        </div>
+    </section>');
+}
+?>
